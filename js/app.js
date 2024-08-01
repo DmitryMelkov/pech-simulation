3cbd53a46e80c4529f1859ddea04958a2229f3c0
Dmitry <melkov.dmitriy40@gmail.com>
2024-08-01 16:47:45 +0500

    add modal

--- js/app.js
+++ js/app.js
@@ -233,6 +233,36 @@ tooltipVisible(thirdSkolzClick, thirdSkolzClue, thirdSkolzClose, thirdSkolzInput
 
 // Функции для инпутов в модалке
 
+const btnModal = document.querySelector('.btn-modal');
+const modalBackground = document.querySelector('.modal-js');
+const modalActive = document.querySelector('.mnemo__modal-start');
+const btnAccept = document.querySelector('.modal-content__form-btn--ok');
+const btnClose = document.querySelector('.mnemo__modal-close');
+
+btnModal.addEventListener('click', () => {
+  modalBackground.classList.add('enabled');
+  modalActive.classList.add('enabled');
+});
+
+modalBackground.addEventListener('click', (event) => {
+  if (event.target === modalBackground) {
+    modalBackground.classList.remove('enabled');
+    modalActive.classList.remove('enabled');
+  }
+});
+
+btnClose.addEventListener('click', () => {
+  modalBackground.classList.remove('enabled');
+  modalActive.classList.remove('enabled');
+})
+
+btnAccept.addEventListener('click', () => {
+  modalBackground.classList.remove('enabled');
+  modalActive.classList.remove('enabled');
+});
+
+
+
 const modalForm = document.querySelector('.all__param-form');
 const modalInputFirstSkolz = document.querySelector('#firstSkolzInputModal');
 const modalInputSecondSkolz = document.querySelector('#secondSkolzInputModal');
